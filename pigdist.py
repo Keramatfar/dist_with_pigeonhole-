a19a8c49b0081fb50dfa6e3810bcdad046b537e8
Keramatfar <keramatfar.a.s@gmail.com>
2022-10-16 16:47:39 +0330

    Update pigdist.py

--- pigdist.py
+++ pigdist.py
@@ -1,6 +1,6 @@
 from math import factorial as fact
 
 def num_of_select_r_from_n_with_atleast_one(n, r):
-    return fact(n-1)/(fact(n-r)*fact(r))
+    return fact(n-1)/(fact(n-r)*fact(r-1))
 
 num_of_select_r_from_n_with_atleast_one(10, 3)
